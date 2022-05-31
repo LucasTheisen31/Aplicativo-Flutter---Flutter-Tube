c26578490584469d84a567bf272d3e47eb75b5fc
LucasTheisen31 <78063228+LucasTheisen31@users.noreply.github.com>
2022-05-31 18:21:40 -0300

    Commit 2 31/05/2022 Lucas Theisen

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
             ),
           )
         ),
-        title: 'Flutter Tube',
+        title: 'Flutter Tube2',
         home: HomePage(),
       ),
     );
